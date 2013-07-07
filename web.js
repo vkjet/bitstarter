a2383fef7a8b3dc57a4744a66baeadedf6183689
Valeriy Kondruk <vk_jet@mail.ru>
2013-07-07 14:58:36 +0000

    index.html output2

--- web.js
+++ web.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer(1024);
+var buf = new Buffer('Hello world from index.html');
 
 var app = express.createServer(express.logger());
 
-len = buf.write(fs.readFileSync('index.html', 'utf-8'));
-
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8', 0, len));
+  response.send(buf.toString('utf-8', 0, fs.readFileSync('index.html')));
 });
 
-Var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
