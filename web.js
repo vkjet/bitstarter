12562414072b4633969270d8da693fc0e749a5a4
Valeriy Kondruk <vk_jet@mail.ru>
2013-07-07 14:49:36 +0000

    index.html output

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer(fs.readFileSync('index.html', 'utf-8'));
+var buf = new Buffer(1024);
 
 var app = express.createServer(express.logger());
 
+len = buf.write(fs.readFileSync('index.html', 'utf-8'));
+
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+  response.send(buf.toString('utf-8', 0, len));
 });
 
 Var port = process.env.PORT || 5000;
