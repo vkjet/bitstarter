018569383043cca84c794ca31737352008df4455
Valeriy Kondruk <vk_jet@mail.ru>
2013-07-07 14:21:24 +0000

    index.html output

--- web.js
+++ web.js
@@ -5,10 +5,10 @@ var buf = new Buffer('test');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html')));
+  response.send(buf.toString(fs.readFileSync('index.html'), 'utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+Var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
